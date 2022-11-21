48acd8d85ad8c9983ae9c433b15a0a8cbf272965
mihyunLee <mihy621@gmail.com>
2022-11-21 23:19:37 +0900

    1000: A+B

---
+++ Nor/11.21/1000.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `1 2`
+).split(" ");
+
+console.log(parseInt(stdin[0]) + parseInt(stdin[1]));
