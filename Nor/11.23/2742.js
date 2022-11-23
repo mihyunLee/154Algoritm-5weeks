255ba7ac51afd3adf2cc9ae8a062eb603429f602
mihyunLee <mihy621@gmail.com>
2022-11-23 19:16:06 +0900

    2742: 기찍 N

---
+++ Nor/11.23/2742.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+const input = Number(
+  process.platform === "linux" ? fs.readFileSync("/dev/stdin").toString() : `5`
+);
+
+let answer = "";
+
+for (let i = input; i >= 1; i--) {
+  answer += `${i}\n`;
+}
+
+console.log(answer);
