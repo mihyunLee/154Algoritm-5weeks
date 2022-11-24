25a8db58d0751ba2c1a4baf51055c57324c0851e
mihyunLee <mihy621@gmail.com>
2022-11-24 17:24:30 +0900

    2439: 별 찍기(2)

---
+++ Nor/11.24/2439.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+const input =
+  process.platform === "linux" ? fs.readFileSync("/dev/stdin").toString() : `5`;
+
+for (let i = 1; i <= input; i++) {
+  console.log(" ".repeat(input - i) + "*".repeat(i));
+}
