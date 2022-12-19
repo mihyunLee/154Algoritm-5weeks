ed83425f3dd7f23089608cfe34477a17baf96b55
mihyunLee <mihy621@gmail.com>
2022-12-19 22:13:16 +0900

    11650: 좌표 정렬하기

---
+++ Nor/12.19/11650.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+
+const input = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `5
+    3 4
+    1 1
+    1 -1
+    2 2
+    3 3`
+)
+  .trim()
+  .split("\n");
+
+const arr = input.slice(1).map((el) => el.trim().split(" ").map(Number));
+
+const answer = arr
+  .sort(function (a, b) {
+    if (a[0] === b[0]) {
+      return a[1] - b[1];
+    }
+
+    return a[0] - b[0];
+  })
+  .map((el) => el.join(" "))
+  .join("\n");
+
+console.log(answer);
